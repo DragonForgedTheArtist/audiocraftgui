004ae97cc493c4ae943c2689611df462eb498880
E. Allen Soard <allensoard@yahoo.com>
2023-06-14 16:34:00 -0700

    Melody based generation script takes json as arguement

--- melody_generation.py
+++ melody_generation.py
@@ -3,27 +3,42 @@ from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 import os
 from utils import generate_random_filename
+import json
+import sys
 
 # Generate audio based on input melody.  Working on a way to work this
 # into the gui
 
+config = {}
+
+if len(sys.argv) < 2:
+    print("Usage: %s <config json file>" % sys.argv[0])
+    print("  See melody_config.json for example")
+    exit()
+
+f = open(sys.argv[1], "r", encoding="utf-8")
+config = json.loads(f.read())
+f.close()
+
+if config.get("melody") is None:
+    print("Malformed json config file")
+    print("  See melody_config.json for example")
+    exit()
+
 temppath = "temp"
-generations = 3
-prompt = '80s pop track with bassy drums and synth, 110 bpm'
-duration = 30
 
 if not os.path.exists(temppath):
     os.makedirs(temppath)
 
 descriptions = []
-for i in range(generations):
-    descriptions.append(prompt)
+for i in range(config['generations']):
+    descriptions.append(config["prompt"])
 
 model = MusicGen.get_pretrained('melody')
-model.set_generation_params(duration=duration)
+model.set_generation_params(duration=config["duration"])
 
-melody_waveform, sr = torchaudio.load("assets/bach.mp3")
-melody_waveform = melody_waveform.unsqueeze(0).repeat(generations, 1, 1)
+melody_waveform, sr = torchaudio.load(config["melody"])
+melody_waveform = melody_waveform.unsqueeze(0).repeat(config["generations"], 1, 1)
 
 output = model.generate_with_chroma(
     descriptions=descriptions,
